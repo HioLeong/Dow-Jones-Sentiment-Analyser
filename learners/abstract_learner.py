b2710cbc3c807f64c99f0d794cc0d52d327b729a
hioleong <hiowengleong@gmail.com>
2015-04-25 19:58:07 +0200

    [Learner] Updated

--- learners/abstract_learner.py
+++ learners/abstract_learner.py
@@ -2,14 +2,14 @@ from abc import ABCMeta, abstractmethod
 
 class DowJonesLearner(object):
     __metaclass__ = ABCMeta
+    self.tweets = []
+
+    def get_tweets_feature_set(self, tweets):
 
     @abstractmethod
     def train(self, training_set):
         pass
 
     @abstractmethod
-    def classify_one(self, tweet):
-        pass
-
-    def classify_many(self, tweets):
+    def classify_tweet(self, tweet_features):
         pass
---
+++ learners/linear_regression.py
@@ -0,0 +1,14 @@
+from abstract_learner import DowJonesLearner
+
+from sklearn import linear_model
+
+class LinearRegressionLearner(DowJonesLearner):
+    def __init__(self, tweets):
+        self.learner = linear_model.LinearRegression()
+        self.tweets = tweets
+
+    def train(self, training_set):
+        self.learner.fit(training_set.features, training_set.labels)
+
+    def classify_tweet(self, tweet_features)
+        self.learner.predict(tweet_features)
