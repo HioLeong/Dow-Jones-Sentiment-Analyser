6374a2648366a52622981b6bd4aa0e1fbc73b130
Rasheed Wihaib <rasheedwihaib@gmail.com>
2015-04-25 19:05:05 +0100

    added bigram feature extractor, adding emoticon extractor

---
+++ learners/featureextractor.py
@@ -0,0 +1,31 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+
+corpus = ["This is going to :) ", "This is an array :(", "of all the tweets :)"];
+bigram_vectorizer = CountVectorizer(ngram_range=(2, 2), token_pattern=r'\b\w+\b', min_df=1)
+
+def getBigrams( corpus ):
+	analyze = bigram_vectorizer.build_analyzer()
+	out = []
+	for s in corpus:
+		out.append(analyze(s))
+	return out
+
+def getVectorizedBigrams( corpus ): 
+	X_2 = bigram_vectorizer.fit_transform(corpus).toarray()
+	return X_2
+
+def getEmoticons( corpus ):
+	out = []
+	for s in corpus:
+		found = re.search('[0-9A-Za-z\&\-\.\/()=:;]+', s).group(1)
+    	if found:
+    		out.append(found)
+    	else:
+    		out.append("")
+	return out
+
+if __name__ == "__main__":
+	print(getEmoticons(corpus))
+	print(getBigrams(corpus))
+	print(getVectorizedBigrams(corpus))
