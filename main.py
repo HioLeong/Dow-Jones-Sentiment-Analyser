5483fafde3e94f8d5940388263965cb056baa70a
hioleong <hiowengleong@gmail.com>
2015-04-26 23:21:53 +0200

    [App] Updated main to get tweets

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ from learners.linear_regression import LinearRegressionLearner
 from learners.logistic_regression import LogisticRegressionLearner
 from learners.naive_bayes import NaiveBayesLearner
 
+from pymongo import MongoClient()
+
+client = MongoClient()
+db = client.dowjones_sa
+
 if __name__ == '__main__':
-    '''
-    tweets = ['hello, is it me', 'are looking for']
+    #tweets = ['hello, is it me', 'are looking for']
+    tweets = db.tweetswithprices.find({},{'text':1, 'price':1})
     labelled_tweets = [tweets, [0,1]]
     lr = LogisticRegressionLearner(labelled_tweets)
     lr.train()
-    '''
 
+    '''
     ts.store_data('data/tweets.csv')
     dj.store_data('data/ChevronCVX.csv', 'chevron')
     dj.store_data('data/ExxonXOM.csv', 'exxon')
     dj.store_data('data/ColaKO.csv', 'coca cola')
+    '''
